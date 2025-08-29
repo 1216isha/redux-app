c092ff681abd3626d969f9aa85e2002389914e62
1216isha <ishanaveed40@gmail.com>
2025-08-29 11:38:51 +0500

    CHanges code

--- src/Components/formData/UserData.jsx
+++ src/Components/formData/UserData.jsx
@@ -1,8 +1,11 @@
 import { useState } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { setUser } from '../../redux/Slice';
 
 const UserData = () => {
+
+    const name = useSelector((state) => state.user.name)
+
 
     const [data, setData] = useState({ name: '', email: '', password: '' })
     const dispatch = useDispatch();
@@ -19,14 +22,15 @@ const UserData = () => {
         e.preventDefault();
         dispatch(setUser(data))
         console.log("User_Data: ", data)
+
     }
 
 
 
     return (
         <div className='w-full py-20 px-10'>
-
-            <form onSubmit={handleSubmit}  className='w-[50%] h-[400px] bg-gray-100 shadow-2xl py-10 px-10  rounded-lg m-auto '>
+{!name &&
+            <form onSubmit={handleSubmit} className='w-[50%] h-[400px] bg-gray-100 shadow-2xl py-10 px-10  rounded-lg m-auto '>
                 <label className='text-xl font-semibold mb-2'>Name: </label>
                 <input type="text" name='name' onChange={handleChange} required value={data.name} className='w-full border rounded-lg py-2 px-2 mb-4 mt-2' />
 
@@ -39,7 +43,7 @@ const UserData = () => {
                 <button type='submit' className='btn py-2 px-4 hover:bg-blue-700 bg-purple-600 text-white rounded-lg text-xl mt-1'>Submit</button>
 
             </form>
-
+ }
         </div>
     )
 }
--- src/Components/formData/UserWelcome.jsx
+++ src/Components/formData/UserWelcome.jsx
@@ -5,9 +5,11 @@ const UserWelcome = () => {
     const  name = useSelector((state) => state.user.name);
   return (
     <div>
+      {name &&
         <div className='w-[40%] bg-gray-100 py-6 px-4 m-auto shadow-2xl rounded-lg mt-20 text-center font-semibold text-xl'>
             <h1>Welcome {name} ✨</h1>
         </div>
+        }
     </div>
   )
 }
