b993e89d27eae2ccfaa05ee100be96dab7cc7326
1216isha <ishanaveed40@gmail.com>
2025-08-29 11:21:18 +0500

    Changes form

--- src/Components/formData/UserData.jsx
+++ src/Components/formData/UserData.jsx
@@ -26,9 +26,9 @@ const UserData = () => {
     return (
         <div className='w-full py-20 px-10'>
 
-            <form className='w-[50%] h-[400px] bg-gray-100 shadow-2xl py-10 px-10  rounded-lg m-auto '>
+            <form onSubmit={handleSubmit}  className='w-[50%] h-[400px] bg-gray-100 shadow-2xl py-10 px-10  rounded-lg m-auto '>
                 <label className='text-xl font-semibold mb-2'>Name: </label>
-                <input type="text" name='name' onChange={handleChange} value={data.name} className='w-full border rounded-lg py-2 px-2 mb-4 mt-2' />
+                <input type="text" name='name' onChange={handleChange} required value={data.name} className='w-full border rounded-lg py-2 px-2 mb-4 mt-2' />
 
                 <label className='text-xl font-semibold mb-2'>Email: </label>
                 <input type="email" name='email' onChange={handleChange} value={data.email} className='w-full border rounded-lg py-2 px-2 mb-4 mt-2' />
@@ -36,7 +36,7 @@ const UserData = () => {
                 <label className='text-xl font-semibold mb-2'>Password: </label>
                 <input type="password" name='password' onChange={handleChange} value={data.password} className='w-full border rounded-lg py-2 px-2 mb-4 mt-2' />
 
-                <button onSubmit={handleSubmit} className='btn py-2 px-4 hover:bg-blue-700 bg-purple-600 text-white rounded-lg text-xl mt-1'>Submit</button>
+                <button type='submit' className='btn py-2 px-4 hover:bg-blue-700 bg-purple-600 text-white rounded-lg text-xl mt-1'>Submit</button>
 
             </form>
 
